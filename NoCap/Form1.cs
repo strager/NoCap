f8ec0a99af6fcd460a2f9efdad03b4f8af928309
strager <strager.nds@gmail.com>
2010-04-04 13:39:52 -0400

    Project...

---
+++ NoCap/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace NoCap {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var img = ScreenCapturer.CaptureEntireDesktop();
+            img.Save(@"img.bmp", ImageFormat.Bmp);
+            MessageBox.Show("done");
+        }
+    }
+}
