fb05dcab1482c17d23744e2ed3b2386041254123
strager <strager.nds@gmail.com>
2010-10-11 07:43:45 -0400

    ClipboardDestination.

---
+++ NoCap.Plugins/ClipboardDestination.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Windows.Forms;
+using NoCap.Library.Destinations;
+
+namespace NoCap.Plugins {
+    [Export(typeof(IDestination))]
+    public class ClipboardDestination : IDestination {
+        public IOperation<TypedData> Put(TypedData data) {
+            return new EasyOperation<TypedData>((op) => {
+                switch (data.Type) {
+                    case TypedDataType.Text:
+                    case TypedDataType.Uri:
+                        Clipboard.SetText(data.Data.ToString(), TextDataFormat.UnicodeText);
+
+                        break;
+
+                    case TypedDataType.Image:
+                        Clipboard.SetImage((Image)data.Data);
+
+                        break;
+
+                    default:
+                        break;
+                }
+
+                return data;
+            });
+        }
+
+        public IEnumerable<TypedDataType> GetInputDataTypes() {
+            return new[] { TypedDataType.Text, TypedDataType.Uri, TypedDataType.Image };
+        }
+
+        public IEnumerable<TypedDataType> GetOutputDataTypes(TypedDataType input) {
+            return new[] { input };
+        }
+    }
+}
