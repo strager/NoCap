cba0ecd5296744b9fa00299f3f796bef05735bbb
strager <strager.nds@gmail.com>
2010-10-11 03:03:17 -0400

    Crop shot.

--- NoCap/Form1.cs
+++ NoCap/Form1.cs
@@ -10,45 +10,43 @@ namespace NoCap {
         private readonly DataRouter router;
         private readonly ISource screenshotSource;
         private readonly ISource clipboardSource;
+        private readonly ISource cropShotSource;
 
         public Form1() {
             InitializeComponent();
 
             this.screenshotSource = new ScreenshotSource(ScreenshotSourceType.EntireDesktop);
             this.clipboardSource = new ClipboardSource();
+            this.cropShotSource = new CropShotSource();
 
             var codecs = ImageCodecInfo.GetImageEncoders().Where(ImageWriter.IsCodecValid);
 
             this.router = new DataRouter();
-            /*router.Routes[TypedDataType.Image] = new DestinationChain(new IDestination[] {
+            router.Routes[TypedDataType.Image] = new DestinationChain(new IDestination[] {
                 new ImageWriter(codecs.FirstOrDefault()),
                 new FileSystemDestination(@".")
-            });*/
-            router.Routes[TypedDataType.Image] = new ImageBinUploader(
+            });
+            /*router.Routes[TypedDataType.Image] = new ImageBinUploader(
                 new ImageWriter(codecs.FirstOrDefault(codec => codec.FormatDescription == "PNG"))
-            );
+            );*/
             router.Routes[TypedDataType.Text] = new SlexyUploader();
             router.Routes[TypedDataType.Uri] = new IsgdShortener();
         }
 
         private void ScreenshotClicked(object sender, EventArgs e) {
-            var sourceOp = this.screenshotSource.Get();
-            sourceOp.Completed += (sender2, e2) => {
-                var destOp = this.router.Put(e2.Data);
-                destOp.Completed += (sender3, e3) => {
-                    Log(e3.Data.ToString());
-                };
-
-                destOp.Start();
-
-                Log(sourceOp.Data.ToString());
-            };
-
-            sourceOp.Start();
+            PerformRequest(this.screenshotSource);
         }
 
         private void ClipboardClicked(object sender, EventArgs e) {
-            var sourceOp = this.clipboardSource.Get();
+            PerformRequest(this.clipboardSource);
+        }
+
+        private void CropShotClicked(object sender, EventArgs e) {
+            PerformRequest(this.cropShotSource);
+        }
+
+        private void PerformRequest(ISource source) {
+            var sourceOp = source.Get();
             sourceOp.Completed += (sender2, e2) => {
                 var destOp = this.router.Put(e2.Data);
                 destOp.Completed += (sender3, e3) => {
---
+++ NoCap/Sources/CropShotSource.cs
@@ -0,0 +1,105 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace NoCap.Sources {
+    public class CropShotSource : ISource {
+        public IOperation<TypedData> Get() {
+            var cropShotForm = new CropShotForm {
+                SourceImage = ScreenCapturer.CaptureEntireDesktop()
+            };
+
+            return cropShotForm.Operation;
+        }
+    }
+
+    public class CropShotForm : Form {
+        public Image SourceImage {
+            get {
+                return BackgroundImage;
+            }
+
+            set {
+                BackgroundImage = value;
+            }
+        }
+
+        private EasyOperation<TypedData> operation;
+
+        public IOperation<TypedData> Operation {
+            get {
+                return this.operation;
+            }
+        }
+
+        private Point dragStart;
+        private bool isDragging;
+
+        public CropShotForm() {
+            this.operation = new EasyOperation<TypedData>((op) => {
+                Show();
+
+                return null;
+            });
+
+            ShowInTaskbar = false;
+
+            Shown     += (sender, e) => PutOnTop();
+            KeyDown   += (sender, e) => KeyPressed(e.KeyCode);
+            LostFocus += (sender, e) => Close();
+
+            Closed += (sender, e) => {
+                if (this.operation.State != OperationState.Completed) {
+                    this.operation.Cancel();
+                }
+            };
+
+            MouseDown += (sender, e) => StartDragging(e.Location);
+            MouseUp   += (sender, e) => EndDragging(e.Location);
+        }
+
+        private void StartDragging(Point location) {
+            isDragging = true;
+            dragStart = location;
+        }
+
+        private void EndDragging(Point location) {
+            if (!isDragging) {
+                return;
+            }
+
+            var selectedImage = GetSelectedImage(Rectangle.FromLTRB(this.dragStart.X, this.dragStart.Y, location.X, location.Y));
+            this.operation.Done(TypedData.FromImage(selectedImage, "crop shot"));
+
+            Close();
+        }
+
+        private Image GetSelectedImage(Rectangle region) {
+            var selectedImage = new Bitmap(region.Width, region.Height);
+            
+            var g = Graphics.FromImage(selectedImage);
+            g.DrawImageUnscaled(SourceImage, -region.X, -region.Y);
+            return selectedImage;
+        }
+
+        private void StopDragging() {
+            isDragging = false;
+        }
+
+        private void KeyPressed(Keys keyCode) {
+            if (keyCode.HasFlag(Keys.Escape)) {
+                if (isDragging) {
+                    StopDragging();
+                }
+
+                Close();
+            }
+        }
+
+        private void PutOnTop() {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Normal;
+            WindowState = FormWindowState.Maximized;
+            Focus();
+        }
+    }
+}
