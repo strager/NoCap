650b09cd71a5e5f4e1e2ff79cb5f3295b6dde9ea
strager <strager.nds@gmail.com>
2010-10-13 19:54:02 -0400

    Progress tracker tests.

---
+++ NoCap.Library.Tests/Util/AggregateProgressTrackerTests.cs
@@ -0,0 +1,72 @@
+﻿using NoCap.Library.Util;
+using NUnit.Framework;
+
+namespace NoCap.Library.Tests.Util {
+    [TestFixture]
+    public class AggregateProgressTrackerTests {
+        [Test]
+        public void ProgressIsAverage() {
+            var npts = new[] {
+                new NotifyingProgressTracker(),
+                new NotifyingProgressTracker(),
+                new NotifyingProgressTracker(),
+                new NotifyingProgressTracker()
+            };
+
+            var apt = new AggregateProgressTracker(npts);
+
+            Assert.AreEqual(0, apt.Progress);
+            
+            npts = new[] {
+                new NotifyingProgressTracker { Progress = 1 },
+                new NotifyingProgressTracker { Progress = 0.5 },
+                new NotifyingProgressTracker { Progress = 0.5 },
+                new NotifyingProgressTracker { Progress = 0 }
+            };
+
+            apt = new AggregateProgressTracker(npts);
+
+            Assert.AreEqual((1 + 0.5 + 0.5 + 0) / 4, apt.Progress);
+
+            npts = new[] {
+                new NotifyingProgressTracker { Progress = 1 },
+                new NotifyingProgressTracker { Progress = 1 },
+                new NotifyingProgressTracker { Progress = 1 },
+                new NotifyingProgressTracker { Progress = 1 }
+            };
+
+            apt = new AggregateProgressTracker(npts);
+
+            Assert.AreEqual(1, apt.Progress);
+        }
+
+        [Test]
+        public void ProgressChangesWhenChildChanges() {
+            var npts = new[] {
+                new NotifyingProgressTracker(),
+                new NotifyingProgressTracker(),
+                new NotifyingProgressTracker(),
+                new NotifyingProgressTracker()
+            };
+
+            var apt = new AggregateProgressTracker(npts);
+
+            Assert.AreEqual(0 / 4.0, apt.Progress);
+            
+            npts[0].Progress = 1;
+            Assert.AreEqual(1 / 4.0, apt.Progress);
+
+            npts[1].Progress = 1;
+            Assert.AreEqual(2 / 4.0, apt.Progress);
+
+            npts[2].Progress = 1;
+            Assert.AreEqual(3 / 4.0, apt.Progress);
+
+            npts[3].Progress = 1;
+            Assert.AreEqual(4 / 4.0, apt.Progress);
+
+            npts[2].Progress = 0.5;
+            Assert.AreEqual(7 / 8.0, apt.Progress);
+        }
+    }
+}
---
+++ NoCap.Library.Tests/Util/NotifyingProgressTrackerTests.cs
@@ -0,0 +1,32 @@
+﻿using NoCap.Library.Util;
+using NUnit.Framework;
+
+namespace NoCap.Library.Tests.Util {
+    [TestFixture]
+    public class NotifyingProgressTrackerTests {
+        [Test]
+        public void ZeroProgressOnNew() {
+            var npt = new NotifyingProgressTracker();
+
+            Assert.AreEqual(0, npt.Progress);
+        }
+
+        [Test]
+        public void NotifyRaised() {
+            var npt = new NotifyingProgressTracker();
+            int progressChangedCallCount = 0;
+
+            npt.PropertyChanged += (sender, e) => {
+                if (e.PropertyName == "Progress") {
+                    progressChangedCallCount += 1;
+                }
+            };
+
+            npt.Progress = 1;
+            Assert.AreEqual(1, progressChangedCallCount);
+
+            npt.Progress = 1;
+            Assert.AreEqual(2, progressChangedCallCount);
+        }
+    }
+}
