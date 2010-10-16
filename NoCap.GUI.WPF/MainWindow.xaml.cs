a7c547565de9c42ba042605bf2e20f3ace569c74
strager <strager.nds@gmail.com>
2010-10-16 01:02:02 -0400

    IT WORKS.

--- NoCap.GUI.WPF/MainWindow.xaml.cs
+++ NoCap.GUI.WPF/MainWindow.xaml.cs
@@ -4,11 +4,13 @@ using System.Collections.ObjectModel;
 using System.ComponentModel.Composition.Hosting;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Windows.Interop;
 using NoCap.GUI.WPF.Settings;
 using NoCap.Library;
 using NoCap.Library.Destinations;
 using NoCap.Library.Util;
 using NoCap.Plugins;
+using WinputDotNet;
 
 namespace NoCap.GUI.WPF {
     /// <summary>
@@ -23,7 +25,22 @@ namespace NoCap.GUI.WPF {
         private readonly IDestination clipboardDestination;
 
         private readonly NotifyingProgressTracker progressTracker;
+
         private ProgramSettings settings;
+
+        private ProgramSettings Settings {
+            get {
+                return this.settings;
+            }
+
+            set {
+                var oldSettings = this.settings;
+
+                this.settings = value;
+
+                OnSettingsChanged(oldSettings, value);
+            }
+        }
 
         public MainWindow() {
             InitializeComponent();
@@ -62,11 +79,60 @@ namespace NoCap.GUI.WPF {
                 this.clipboardDestination
             );
 
-            this.settings = new ProgramSettings {
+            Settings = new ProgramSettings {
                 Commands = new ObservableCollection<SourceDestinationCommand>(new List<SourceDestinationCommand> {
-                    new SourceDestinationCommand("Clipboard", this.screenshotSource, this.clipboardDestination),
+                    new SourceDestinationCommand("Clipboard", this.clipboardSource, this.router),
                 })
             };
+        }
+
+        private void OnSettingsChanged(ProgramSettings oldSettings, ProgramSettings newSettings) {
+            if (oldSettings != null) {
+                ShutDownEverything(oldSettings);
+            }
+
+            if (newSettings != null) {
+                SetUpEverything(newSettings);
+            }
+        }
+
+        private void SetUpEverything(ProgramSettings newSettings) {
+            SetUpInput(newSettings);
+        }
+
+        private void ShutDownEverything(ProgramSettings oldSettings) {
+            ShutDownInput(oldSettings);
+        }
+
+        private void SetUpInput(ProgramSettings newSettings) {
+            var inputProvider = newSettings.InputProvider;
+
+            if (inputProvider == null) {
+                return;
+            }
+
+            var handle = new WindowInteropHelper(this).Handle;
+            
+            inputProvider.CommandStateChanged += CommandStateChanged;
+            inputProvider.SetBindings(newSettings.Bindings);
+            inputProvider.Attach(handle);
+        }
+
+        private void ShutDownInput(ProgramSettings oldSettings) {
+            var inputProvider = oldSettings.InputProvider;
+
+            if (inputProvider == null) {
+                return;
+            }
+
+            inputProvider.Detach();
+            inputProvider.CommandStateChanged -= CommandStateChanged;
+        }
+
+        private void CommandStateChanged(object sender, CommandStateChangedEventArgs e) {
+            var command = (SourceDestinationCommand) e.Command;
+
+            PerformRequestAsync(command);
         }
 
         private void SetProgress(double progress) {
@@ -86,10 +152,14 @@ namespace NoCap.GUI.WPF {
         }
 
         private void SettingsClicked(object sender, EventArgs e) {
-            var settingsWindow = new SettingsWindow(this.settings.Clone());
-            
+            var settingsWindow = new SettingsWindow(Settings.Clone());
+
+            ShutDownInput(this.settings);
+
             if (settingsWindow.ShowDialog() == true) {
-                this.settings = settingsWindow.ProgramSettings;
+                Settings = settingsWindow.ProgramSettings;
+            } else {
+                SetUpInput(this.settings);
             }
         }
 
@@ -100,6 +170,28 @@ namespace NoCap.GUI.WPF {
                 System.Diagnostics.Debug.WriteLine(this.progressTracker.Progress.ToString());
                 routeFromAsync.EndInvoke(ar);
             }, null);
+        }
+
+        private void PerformRequestSync(SourceDestinationCommand command, IMutableProgressTracker progress) {
+            var sourceProgress = new NotifyingProgressTracker();
+            var destProgress = new NotifyingProgressTracker();
+
+            var aggregateProgress = new AggregateProgressTracker(sourceProgress, destProgress);
+            aggregateProgress.BindTo(progress);
+
+            var data = command.Source.Get(sourceProgress);
+
+            if (data == null) {
+                return;
+            }
+
+            command.Destination.Put(data, destProgress);
+        }
+
+        private void PerformRequestAsync(SourceDestinationCommand command) {
+            var func = new Action<SourceDestinationCommand, IMutableProgressTracker>(PerformRequestSync);
+
+            func.BeginInvoke(command, this.progressTracker, func.EndInvoke, null);
         }
 
         private void Log(string format, params object[] args) {
