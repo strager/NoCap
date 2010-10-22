fd37fbc36ff4d7f629940ef50911f8c382fb5bd5
strager <strager.nds@gmail.com>
2010-10-22 14:27:55 -0400

    ProcessorChain tests.

---
+++ NoCap.Library.Tests/Processors/ProcessorChainTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Moq;
+using NoCap.Library.Processors;
+using NoCap.Library.Util;
+using NUnit.Framework;
+
+namespace NoCap.Library.Tests.Processors {
+    [TestFixture]
+    class ProcessorChainTests {
+        [Test]
+        public void RouteChecksTypes1() {
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processorMock = GetProcessorMock();
+            processorMock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processorMock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns((TypedData) null);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processorMock.Object);
+
+            dataRouter.Process(inputData, inputTracker);
+
+            processorMock.Verify((processor) => processor.GetInputDataTypes(), Times.AtLeastOnce());
+        }
+        
+        [Test]
+        public void RouteThrowsOnTypeMismatch1() {
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processorMock = GetProcessorMock();
+            processorMock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Uri });
+            processorMock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns((TypedData) null);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processorMock.Object);
+
+            Assert.Throws<InvalidOperationException>(() => dataRouter.Process(inputData, inputTracker));
+        }
+        
+        [Test]
+        public void RouteReturnsData1() {
+            var expectedOutput = GetTextData();
+
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processorMock = GetProcessorMock();
+            processorMock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processorMock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns(expectedOutput);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processorMock.Object);
+
+            var actualData = dataRouter.Process(inputData, inputTracker);
+            Assert.AreSame(expectedOutput, actualData);
+        }
+        
+        [Test]
+        public void RouteChecksTypes2() {
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processor1Mock = GetProcessorMock();
+            processor1Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processor1Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns(inputData);
+
+            var processor2Mock = GetProcessorMock();
+            processor2Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processor2Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns((TypedData) null);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processor1Mock.Object);
+            dataRouter.Add(processor2Mock.Object);
+
+            dataRouter.Process(inputData, inputTracker);
+            
+            processor1Mock.Verify((processor) => processor.GetInputDataTypes(), Times.AtLeastOnce());
+            processor2Mock.Verify((processor) => processor.GetInputDataTypes(), Times.AtLeastOnce());
+        }
+
+        [Test]
+        public void RouteThrowsOnTypeMismatch2A() {
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processor1Mock = GetProcessorMock();
+            processor1Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Uri });
+            processor1Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns(inputData);
+
+            var processor2Mock = GetProcessorMock();
+            processor2Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processor2Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns((TypedData) null);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processor1Mock.Object);
+            dataRouter.Add(processor2Mock.Object);
+
+            Assert.Throws<InvalidOperationException>(() => dataRouter.Process(inputData, inputTracker));
+        }
+
+        [Test]
+        public void RouteThrowsOnTypeMismatch2B() {
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processor1Mock = GetProcessorMock();
+            processor1Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processor1Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns(inputData);
+
+            var processor2Mock = GetProcessorMock();
+            processor2Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Uri });
+            processor2Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns((TypedData) null);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processor1Mock.Object);
+            dataRouter.Add(processor2Mock.Object);
+
+            Assert.Throws<InvalidOperationException>(() => dataRouter.Process(inputData, inputTracker));
+        }
+
+        [Test]
+        public void RouteReturnsData2() {
+            var expectedOutput = GetTextData();
+
+            var inputTracker = GetMutableProgressTracker();
+            var inputData = GetTextData();
+
+            var processor1Mock = GetProcessorMock();
+            processor1Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processor1Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns(inputData);
+
+            var processor2Mock = GetProcessorMock();
+            processor2Mock.Setup((processor) => processor.GetInputDataTypes()).Returns(new[] { TypedDataType.Text });
+            processor2Mock.Setup((processor) => processor.Process(inputData, It.IsAny<IMutableProgressTracker>())).Returns(expectedOutput);
+
+            var dataRouter = new ProcessorChain();
+            dataRouter.Add(processor1Mock.Object);
+            dataRouter.Add(processor2Mock.Object);
+
+            var actualOutput = dataRouter.Process(inputData, inputTracker);
+            Assert.AreSame(expectedOutput, actualOutput);
+        }
+
+        private static Mock<IProcessor> GetProcessorMock() {
+            return new Mock<IProcessor>(MockBehavior.Strict);
+        }
+
+        private static NotifyingProgressTracker GetMutableProgressTracker() {
+            return new NotifyingProgressTracker();
+        }
+
+        private static TypedData GetTextData() {
+            return TypedData.FromText("foobar", "test text");
+        }
+    }
+}
--- NoCap.Library/Processors/ProcessorChain.cs
+++ NoCap.Library/Processors/ProcessorChain.cs
@@ -1,48 +1,40 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NoCap.Library.Util;
 
 namespace NoCap.Library.Processors {
-    public class ProcessorChain : IProcessor {
-        private readonly List<IProcessor> destinations = new List<IProcessor>();
+    public class ProcessorChain : IProcessor, IList<IProcessor> {
+        private readonly List<IProcessor> processors = new List<IProcessor>();
 
         public string Name {
             get { return "Destination chain"; }
-        }
-
-        public IList<IProcessor> Destinations {
-            get {
-                return this.destinations;
-            }
         }
 
         public ProcessorChain() {
         }
 
         public ProcessorChain(params IProcessor[] processors) :
-            this((IEnumerable<IProcessor>)processors) {
+            this((IEnumerable<IProcessor>) processors) {
         }
 
-        public ProcessorChain(IEnumerable<IProcessor> destinations) {
-            this.destinations.AddRange(destinations);
+        public ProcessorChain(IEnumerable<IProcessor> processors) {
+            this.processors.AddRange(processors);
         }
 
         public TypedData Process(TypedData data, IMutableProgressTracker progress) {
             // ToList is needed for some strange reason
-            var progressTrackers = this.destinations.Select((destination) => new NotifyingProgressTracker()).ToList();
+            var progressTrackers = this.processors.Select((destination) => new NotifyingProgressTracker()).ToList();
             var aggregateProgress = new AggregateProgressTracker(progressTrackers);
             aggregateProgress.BindTo(progress);
 
             using (var trackerEnumerator = progressTrackers.GetEnumerator()) {
-                foreach (var destination in Destinations) {
+                foreach (var destination in this.processors) {
                     trackerEnumerator.MoveNext();
-                    data = destination.Process(data, trackerEnumerator.Current);
 
-                    if (data == null) {
-                        // FIXME Maybe throw ...?
-                        break;
-                    }
+                    destination.CheckValidInputType(data);
+
+                    data = destination.Process(data, trackerEnumerator.Current);
                 }
             }
 
@@ -50,23 +42,84 @@ namespace NoCap.Library.Processors {
         }
 
         public IEnumerable<TypedDataType> GetInputDataTypes() {
-            if (!this.destinations.Any()) {
-                return new TypedDataType[] {
-                };
+            if (!this.processors.Any()) {
+                return new TypedDataType[] { };
             }
 
-            return this.destinations.First().GetInputDataTypes();
+            return this.processors.First().GetInputDataTypes();
         }
 
         public IEnumerable<TypedDataType> GetOutputDataTypes(TypedDataType input) {
-            return this.destinations.Aggregate(
-                (IEnumerable<TypedDataType>)new[] { input },
+            return this.processors.Aggregate(
+                (IEnumerable<TypedDataType>) new[] { input },
                 (types, destination) => types.SelectMany(destination.GetOutputDataTypes).Unique()
             );
         }
 
         public IProcessorFactory GetFactory() {
             return null;
+        }
+
+        public IEnumerator<IProcessor> GetEnumerator() {
+            return this.processors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        public void Add(IProcessor item) {
+            this.processors.Add(item);
+        }
+
+        public void Clear() {
+            this.processors.Clear();
+        }
+
+        public bool Contains(IProcessor item) {
+            return this.processors.Contains(item);
+        }
+
+        public void CopyTo(IProcessor[] array, int arrayIndex) {
+            this.processors.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(IProcessor item) {
+            return this.processors.Remove(item);
+        }
+
+        public int Count {
+            get {
+                return this.processors.Count;
+            }
+        }
+
+        bool ICollection<IProcessor>.IsReadOnly {
+            get {
+                return false;
+            }
+        }
+
+        public int IndexOf(IProcessor item) {
+            return this.processors.IndexOf(item);
+        }
+
+        public void Insert(int index, IProcessor item) {
+            this.processors.Insert(index, item);
+        }
+
+        public void RemoveAt(int index) {
+            this.processors.RemoveAt(index);
+        }
+
+        public IProcessor this[int index] {
+            get {
+                return this.processors[index];
+            }
+
+            set {
+                this.processors[index] = value;
+            }
         }
     }
 }
