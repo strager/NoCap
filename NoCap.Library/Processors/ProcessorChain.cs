872d5da138195b908417fd962469b449306b0fd6
strager <strager.nds@gmail.com>
2010-10-25 04:51:48 -0400

    Make ProcessorChain not implement IList<IProcessor>.

--- NoCap.Library/Processors/ProcessorChain.cs
+++ NoCap.Library/Processors/ProcessorChain.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NoCap.Library.Util;
 
 namespace NoCap.Library.Processors {
-    public class ProcessorChain : IProcessor, IList<IProcessor> {
-        private readonly List<IProcessor> processors = new List<IProcessor>();
+    public class ProcessorChain : IProcessor {
+        public IList<IProcessor> Processors {
+            get;
+            set;
+        }
 
         public string Name {
             get { return "Destination chain"; }
         }
 
         public ProcessorChain() {
+            Processors = new List<IProcessor>();
         }
 
         public ProcessorChain(params IProcessor[] processors) :
@@ -19,17 +22,17 @@ namespace NoCap.Library.Processors {
         }
 
         public ProcessorChain(IEnumerable<IProcessor> processors) {
-            this.processors.AddRange(processors);
+            Processors = processors.ToList();
         }
 
         public TypedData Process(TypedData data, IMutableProgressTracker progress) {
             // ToList is needed for some strange reason
-            var progressTrackers = this.processors.Select((destination) => new NotifyingProgressTracker()).ToList();
+            var progressTrackers = Processors.Select((destination) => new NotifyingProgressTracker()).ToList();
             var aggregateProgress = new AggregateProgressTracker(progressTrackers);
             aggregateProgress.BindTo(progress);
 
             using (var trackerEnumerator = progressTrackers.GetEnumerator()) {
-                foreach (var destination in this.processors) {
+                foreach (var destination in Processors) {
                     trackerEnumerator.MoveNext();
 
                     destination.CheckValidInputType(data);
@@ -42,21 +45,21 @@ namespace NoCap.Library.Processors {
         }
 
         public IEnumerable<TypedDataType> GetInputDataTypes() {
-            if (!this.processors.Any()) {
+            if (!Processors.Any()) {
                 return new TypedDataType[] { };
             }
 
-            return this.processors.First().GetInputDataTypes();
+            return Processors.First().GetInputDataTypes();
         }
 
         public IEnumerable<TypedDataType> GetOutputDataTypes(TypedDataType input) {
-            if (!this.processors.Any()) {
+            if (!Processors.Any()) {
                 return new[] { input };
             }
 
             this.CheckValidInputType(input);
 
-            return GetChainOutputDataTypes(input, this.processors).Unique();
+            return GetChainOutputDataTypes(input, Processors).Unique();
         }
 
         private static IEnumerable<TypedDataType> GetChainOutputDataTypes(TypedDataType input, IEnumerable<IProcessor> processors) {
@@ -67,13 +70,13 @@ namespace NoCap.Library.Processors {
             var processor = processors.First();
 
             if (!processor.IsValidInputType(input)) {
-                return new TypedDataType[] { };
+                return Enumerable.Empty<TypedDataType>();
             }
 
             var outputTypes = processor.GetOutputDataTypes(input);
 
             return outputTypes.Aggregate(
-                (IEnumerable<TypedDataType>) new TypedDataType[] { },
+                Enumerable.Empty<TypedDataType>(),
                 (types, type) => types.Concat(GetChainOutputDataTypes(type, processors.Skip(1)))
             );
         }
@@ -82,66 +85,8 @@ namespace NoCap.Library.Processors {
             return null;
         }
 
-        public IEnumerator<IProcessor> GetEnumerator() {
-            return this.processors.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() {
-            return GetEnumerator();
-        }
-
         public void Add(IProcessor item) {
-            this.processors.Add(item);
-        }
-
-        public void Clear() {
-            this.processors.Clear();
-        }
-
-        public bool Contains(IProcessor item) {
-            return this.processors.Contains(item);
-        }
-
-        public void CopyTo(IProcessor[] array, int arrayIndex) {
-            this.processors.CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(IProcessor item) {
-            return this.processors.Remove(item);
-        }
-
-        public int Count {
-            get {
-                return this.processors.Count;
-            }
-        }
-
-        bool ICollection<IProcessor>.IsReadOnly {
-            get {
-                return false;
-            }
-        }
-
-        public int IndexOf(IProcessor item) {
-            return this.processors.IndexOf(item);
-        }
-
-        public void Insert(int index, IProcessor item) {
-            this.processors.Insert(index, item);
-        }
-
-        public void RemoveAt(int index) {
-            this.processors.RemoveAt(index);
-        }
-
-        public IProcessor this[int index] {
-            get {
-                return this.processors[index];
-            }
-
-            set {
-                this.processors[index] = value;
-            }
+            Processors.Add(item);
         }
     }
 }
