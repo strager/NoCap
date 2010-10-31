96e24b8cb7690b2900377029e7fc727ce3cb305d
strager <strager.nds@gmail.com>
2010-10-31 05:16:27 -0400

    Code cleanup.

--- NoCap.GUI.WPF/App.xaml.cs
+++ NoCap.GUI.WPF/App.xaml.cs
@@ -1,26 +1,175 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Hardcodet.Wpf.TaskbarNotification;
+using NoCap.GUI.WPF.Commands;
+using NoCap.GUI.WPF.Settings;
+using NoCap.Library;
+using NoCap.Library.Util;
+using WinputDotNet;
+using ICommand = NoCap.Library.ICommand;
 
 namespace NoCap.GUI.WPF {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App {
-        private Program program;
+        private IMutableProgressTracker progressTracker;
+        private TaskbarIcon taskbarIcon;
 
-        protected override void OnStartup(System.Windows.StartupEventArgs e) {
-            base.OnStartup(e);
+        private ProgramSettings settings;
+        private ProgramSettingsInfoStuff infoStuff;
+        private SettingsWindow settingsWindow;
 
-            ShutdownMode = System.Windows.ShutdownMode.OnExplicitShutdown;
+        public ProgramSettings Settings {
+            get {
+                return this.settings;
+            }
 
-            this.program = new Program();
-            this.program.Run();
+            private set {
+                var oldSettings = this.settings;
+
+                this.settings = value;
+
+                OnSettingsChanged(oldSettings, value);
+            }
         }
 
-        [STAThread]
-        static void Main() {
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
+        private void Load() {
+            this.progressTracker = new NotifyingProgressTracker();
+            this.taskbarIcon = (TaskbarIcon) Resources["taskbarIcon"];
+
+            LoadBindings();
+            LoadSettings();
+        }
+
+        private void LoadBindings() {
+            // FIXME Make close binding work!
+            // This binding doesn't work (if uncommented and if matching XAML uncommented).
+            // The menu item shows as disabled.
+            //
+            // var closeBinding = new System.Windows.Input.CommandBinding(ApplicationCommands.Close);
+            // closeBinding.Executed += (sender, e) => Shutdown(0);
+            // this.taskbarIcon.CommandBindings.Add(closeBinding);
+
+            var settingsBinding = new System.Windows.Input.CommandBinding(ApplicationCommands.Properties);
+            settingsBinding.Executed += (sender, e) => ShowSettingsEditor();
+            this.taskbarIcon.CommandBindings.Add(settingsBinding);
+        }
+
+        private void LoadSettings() {
+            Settings = new ProgramSettings();
+
+            this.infoStuff = new ProgramSettingsInfoStuff(Settings, Providers.Instance);
+
+            Settings.Commands = new ObservableCollection<ICommand>(
+                Providers.Instance.ProcessorFactories
+                    .Where((factory) => factory.CommandFeatures.HasFlag(CommandFeatures.StandAlone))
+                    .Select((factory) => factory.CreateCommand(this.infoStuff))
+            );
+        }
+
+        private void OnSettingsChanged(ProgramSettings oldSettings, ProgramSettings newSettings) {
+            if (oldSettings != null) {
+                ShutDownEverything(oldSettings);
+            }
+
+            if (newSettings != null) {
+                SetUpEverything(newSettings);
+            }
+        }
+
+        private void SetUpEverything(ProgramSettings newSettings) {
+            SetUpInput(newSettings);
+        }
+
+        private void ShutDownEverything(ProgramSettings oldSettings) {
+            ShutDownInput(oldSettings);
+        }
+
+        private void SetUpInput(ProgramSettings newSettings) {
+            var inputProvider = newSettings.InputProvider;
+
+            if (inputProvider == null) {
+                return;
+            }
+
+            var handle = new IntPtr(42);    // Hope this doesn't crash.
+            
+            inputProvider.CommandStateChanged += CommandStateChanged;
+            inputProvider.SetBindings(newSettings.Bindings);
+            inputProvider.Attach(handle);
+        }
+
+        private void ShutDownInput(ProgramSettings oldSettings) {
+            var inputProvider = oldSettings.InputProvider;
+
+            if (inputProvider == null) {
+                return;
+            }
+
+            inputProvider.Detach();
+            inputProvider.CommandStateChanged -= CommandStateChanged;
+        }
+
+        private void CommandStateChanged(object sender, CommandStateChangedEventArgs e) {
+            if (e.State == InputState.On) {
+                var command = (BoundCommand) e.Command;
+
+                PerformRequestAsync(command.Command);
+            }
+        }
+
+        private void SetProgress(double progress) {
+            //var setProgress = new Action(() => this.progressBar.Value = progress);
+
+            //setProgress.BeginInvoke(setProgress.EndInvoke, null);
+        }
+
+        private void ShowSettingsEditor() {
+            if (this.settingsWindow != null) {
+                this.settingsWindow.Show();
+            }
+
+            this.settingsWindow = new SettingsWindow(Settings.Clone());
+            this.settingsWindow.Closed += (sender, e) => CheckSettingsEditorResult();
+            this.settingsWindow.Show();
+        }
+
+        private void CheckSettingsEditorResult() {
+            if (this.settingsWindow.DialogResult == true) {
+                ShutDownInput(this.settings);
+                Settings = this.settingsWindow.ProgramSettings;
+                SetUpInput(this.settings);
+            }
+
+            this.settingsWindow.Close();
+            this.settingsWindow = null;
+        }
+
+        private static void PerformRequestSync(ICommand highLevelCommand, IMutableProgressTracker progress) {
+            highLevelCommand.Process(null, progress);
+        }
+
+        private void PerformRequestAsync(ICommand highLevelCommand) {
+            var func = new Action<ICommand, IMutableProgressTracker>(PerformRequestSync);
+
+            func.BeginInvoke(highLevelCommand, this.progressTracker, func.EndInvoke, null);
+        }
+
+        public void Start() {
+            ShowSettingsEditor();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e) {
+            Load();
+            Start();
+        }
+
+        private void ExitClicked(object sender, RoutedEventArgs e) {
+            Shutdown(0);
         }
     }
 }
