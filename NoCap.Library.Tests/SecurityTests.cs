56b9721fcdf1796fcdd34240b1f08367a47beb40
strager <strager.nds@gmail.com>
2010-11-06 10:08:24 -0400

    Security tests.

---
+++ NoCap.Library.Tests/SecurityTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace NoCap.Library.Tests {
+    [TestFixture]
+    class SecurityTests {
+        [Test]
+        public void EncryptDecrypt() {
+            const string input = "foobar";
+
+            string output =
+                Security.ToInsecureString(
+                    Security.DecryptString(
+                        Security.EncryptString(
+                            Security.ToSecureString(
+                                input
+                            )
+                        )
+                    )
+                );
+
+            Assert.AreEqual(input, output);
+        }
+
+        [Test]
+        public void SecureInsecure() {
+            const string input = "foobar";
+
+            string output = Security.ToInsecureString(Security.ToSecureString(input));
+
+            Assert.AreEqual(input, output);
+        }
+    }
+}
