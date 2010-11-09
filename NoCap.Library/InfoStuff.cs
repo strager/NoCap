2d837f776d225d954413cc564293d86334b16cf9
strager <strager.nds@gmail.com>
2010-11-09 15:28:37 -0500

    Remove unused fuctions.

--- NoCap.Library/InfoStuff.cs
+++ NoCap.Library/InfoStuff.cs
@@ -1,20 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace NoCap.Library {
     public static class InfoStuff {
-        public static IEnumerable<ICommand> GetTextUploaders(this IInfoStuff infoStuff) {
-            return infoStuff.Commands.Where((command) => command.GetFactory().CommandFeatures.HasFlag(CommandFeatures.TextUploader));
-        }
-        
-        public static IEnumerable<ICommand> GetImageUploaders(this IInfoStuff infoStuff) {
-            return infoStuff.Commands.Where((command) => command.GetFactory().CommandFeatures.HasFlag(CommandFeatures.ImageUploader));
-        }
-        
-        public static IEnumerable<ICommand> GetUrlShorteners(this IInfoStuff infoStuff) {
-            return infoStuff.Commands.Where((command) => command.GetFactory().CommandFeatures.HasFlag(CommandFeatures.UrlShortener));
-        }
-        
         public static ICommand GetDefaultCommand(this IInfoStuff infoStuff, CommandFeatures features) {
             var sharedCommands = infoStuff.Commands.Where((command) => command.GetFactory().CommandFeatures.HasFlag(features));
 
