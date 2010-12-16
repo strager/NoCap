ce891e892216a52cf26aba59d8d3d4024bb179db
strager <strager.nds@gmail.com>
2010-12-16 05:37:47 -0500

    postimage.org uploader.

---
+++ NoCap.Extensions.Default/Commands/PostImageUploader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using NoCap.Extensions.Default.Factories;
+using NoCap.Library;
+using NoCap.Library.Commands;
+using NoCap.Library.Imaging;
+using NoCap.Web.Multipart;
+
+namespace NoCap.Extensions.Default.Commands {
+    [Serializable]
+    class PostImageUploader : ImageUploader {
+        private static readonly Regex LinkInHtml = new Regex(
+            @"http://postimage.org/image/.*?/",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled
+        );
+
+        public override string Name {
+            get {
+                return "postimage.org uploader";
+            }
+        }
+
+        protected override Uri Uri {
+            get {
+                return new Uri("http://www.postimage.org/", UriKind.Absolute);
+            }
+        }
+
+        protected override void PreprocessRequestData(MultipartBuilder helper, TypedData originalData) {
+            helper.File((Stream) originalData.Data, "upload", originalData.Name);
+        }
+
+        public PostImageUploader(ImageWriter writer)
+            : base(writer) {
+            IsAdult = true;
+        }
+
+        protected override IDictionary<string, string> GetParameters(TypedData data) {
+            return new Dictionary<string, string> {
+                // FIXME Remove and adjust as required.  Just copy-pasted all
+                // this from the main page.  (Don't shoot me.)
+                { "mode", "local" },
+                { "formurl", "http://www.postimage.org/" },
+                { "tpl", "." },
+                { "addform", "" },
+                { "mforum", "" },
+                { "um", "image" },
+                { "adult", IsAdult ? "yes" : "no" },
+                { "ui", "" },
+                { "hash", "943" },
+                { "optsize", "0" },
+                { "submit", "Upload It!" },
+            };
+        }
+
+        public bool IsAdult {
+            get;
+            set;
+        }
+
+        protected override TypedData GetResponseData(HttpWebResponse response, TypedData originalData) {
+            string html = GetResponseText(response);
+
+            // Sorry for using regexp to parse HTML, but
+            // including an HTML parsing library as a
+            // dependency is just too much
+
+            var match = LinkInHtml.Match(html);
+
+            if (!match.Success) {
+                return null;
+            }
+
+            return TypedData.FromUri(match.Value, originalData.Name);
+        }
+
+        public override ICommandFactory GetFactory() {
+            return new PostImageUploaderFactory();
+        }
+    }
+}
---
+++ NoCap.Extensions.Default/Factories/PostImageUploaderFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using NoCap.Extensions.Default.Commands;
+using NoCap.Extensions.Default.Editors;
+using NoCap.Library;
+using NoCap.Library.Imaging;
+
+namespace NoCap.Extensions.Default.Factories {
+    [Export(typeof(ICommandFactory))]
+    class PostImageUploaderFactory : ICommandFactory {
+        public string Name {
+            get {
+                return "postimage.org uploader";
+            }
+        }
+
+        public ICommand CreateCommand() {
+            return new PostImageUploader(new ImageWriter { Codec = new PngBitmapCodec() });
+        }
+
+        public void PopulateCommand(ICommand command, ICommandProvider commandProvider) {
+        }
+
+        public ICommandEditor GetCommandEditor(ICommandProvider commandProvider) {
+            return new PostImageUploaderEditor();
+        }
+
+        public CommandFeatures CommandFeatures {
+            get {
+                return CommandFeatures.ImageUploader;
+            }
+        }
+    }
+}
