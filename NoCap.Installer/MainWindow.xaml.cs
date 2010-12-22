fd8f041d27337188c5e3a22e7223b2fc97ba53f6
strager <strager.nds@gmail.com>
2010-12-22 08:35:01 -0500

    Installer files.

--- NoCap.Installer/MainWindow.xaml.cs
+++ NoCap.Installer/MainWindow.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace NoCap.Installer {
+﻿using System.IO;
+
+namespace NoCap.Installer {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
+
+            var zipData = Properties.Resources.InstallFiles;
+
+            using (var zipStream = new MemoryStream(zipData)) {
+                var zip = new Chiron.ZipArchive(zipStream, FileAccess.Read);
+                var files = zip.Files;
+            }
         }
     }
 }
