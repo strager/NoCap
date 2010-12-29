6ffda06af9ed67bd9c1e044812e4dd2e29113b5a
strager <strager.nds@gmail.com>
2010-12-29 04:16:55 -0500

    Remove dead code and clearer helper names.

---
+++ NoCap.GUI.WPF/Util/ExtensionDataContractResolver.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Xml;
+using NoCap.GUI.WPF.Runtime;
+
+namespace NoCap.GUI.WPF.Util {
+    class ExtensionDataContractResolver : DataContractResolver {
+        private readonly ExtensionManager extensionManager;
+
+        public ExtensionDataContractResolver(ExtensionManager extensionManager) {
+            this.extensionManager = extensionManager;
+        }
+
+        public override bool TryResolveType(Type type, Type declaredType, DataContractResolver knownTypeResolver, out XmlDictionaryString typeName, out XmlDictionaryString typeNamespace) {
+            var dictionary = new XmlDictionary();
+
+            string typeNameString = GetDataContractName(type);
+            string typeNamespaceString = GetDataContractNamespace(type);
+
+            if (typeNameString == null || typeNamespaceString == null) {
+                return knownTypeResolver.TryResolveType(type, declaredType, knownTypeResolver, out typeName, out typeNamespace);
+            }
+
+            typeName = dictionary.Add(typeNameString);
+            typeNamespace = dictionary.Add(typeNamespaceString);
+
+            return true;
+        }
+
+        private static string GetDataContractNamespace(Type type) {
+            var dataContractAttributes = type.GetCustomAttributes(typeof(DataContractAttribute), true).OfType<DataContractAttribute>();
+            var namespaceAttr = dataContractAttributes.FirstOrDefault((attr) => attr.Namespace != null);
+
+            if (namespaceAttr != null) {
+                return namespaceAttr.Namespace;
+            }
+
+            var namespaceAttributes = type.Assembly.GetCustomAttributes(typeof(ContractNamespaceAttribute), false).OfType<ContractNamespaceAttribute>();
+            var namespaceAttr2 = namespaceAttributes.FirstOrDefault((attr) => attr.ClrNamespace == type.Namespace);
+
+            return namespaceAttr2 == null ? null : namespaceAttr2.ContractNamespace;
+        }
+
+        private static string GetDataContractName(Type type) {
+            var dataContractAttributes = type.GetCustomAttributes(typeof(DataContractAttribute), true).OfType<DataContractAttribute>();
+            var nameAttr = dataContractAttributes.FirstOrDefault((attr) => attr.Name != null);
+
+            return nameAttr == null ? null : nameAttr.Name;
+        }
+
+        public override Type ResolveName(string typeName, string typeNamespace, Type declaredType, DataContractResolver knownTypeResolver) {
+            var assemblies = GetNamespaceAssemblies(typeNamespace);
+
+            foreach (var assembly in assemblies) {
+                var type = assembly.GetTypes().Where((t) => IsTypeContractMatch(t, typeName, typeNamespace)).SingleOrDefault();
+
+                if (type != null) {
+                    return type;
+                }
+            }
+
+            return knownTypeResolver.ResolveName(typeName, typeNamespace, declaredType, knownTypeResolver);
+        }
+
+        private static bool IsTypeContractMatch(Type type, string typeName, string typeNamespace) {
+            return GetDataContractName(type) == typeName && GetDataContractNamespace(type) == typeNamespace;
+        }
+
+        private IEnumerable<Assembly> GetNamespaceAssemblies(string typeNamespace) {
+            // TODO Clean up
+
+            var extension = this.extensionManager.Extensions.Where((ext) => ext.Namespace == typeNamespace).SingleOrDefault();
+
+            if (extension == null) {
+                // Not a type defined in an extension; test NoCap.Library.
+                if (typeNamespace == "http://strager.net/nocap/lib") {
+                    return new[] { typeof(NoCap.Library.ICommand).Assembly };
+                }
+
+                if (typeNamespace == "http://strager.net/nocap/gui") {
+                    return new[] { typeof(App).Assembly };
+                }
+
+                return Enumerable.Empty<Assembly>();
+            }
+
+            return extension.Assemblies;
+        }
+    }
+}
--- NoCap.GUI.WPF/Util/ProgramSettingsDataSerializer.cs
+++ NoCap.GUI.WPF/Util/ProgramSettingsDataSerializer.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
-using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Xml;
-using Bindable.Linq;
 using System.Linq;
 using NoCap.GUI.WPF.Runtime;
 using NoCap.GUI.WPF.Settings;
-using NoCap.Library.Util;
 
 namespace NoCap.GUI.WPF.Util {
     class ProgramSettingsDataSerializer {
@@ -37,8 +30,8 @@ namespace NoCap.GUI.WPF.Util {
                 int.MaxValue,
                 false,
                 true,
-                new MySurrogate(),
-                new MyDataContractResolver(this.extensionManager)
+                new SafeRoundTripSurrogate(),
+                new ExtensionDataContractResolver(this.extensionManager)
             );
         }
 
@@ -97,182 +90,6 @@ namespace NoCap.GUI.WPF.Util {
 
                 return (T) cloner.Deserialize(stream);
             }
-        }
-    }
-
-    class MyDataContractResolver : DataContractResolver {
-        private readonly ExtensionManager extensionManager;
-
-        public MyDataContractResolver(ExtensionManager extensionManager) {
-            this.extensionManager = extensionManager;
-        }
-
-        public override bool TryResolveType(Type type, Type declaredType, DataContractResolver knownTypeResolver, out XmlDictionaryString typeName, out XmlDictionaryString typeNamespace) {
-            var dictionary = new XmlDictionary();
-
-            string typeNameString = GetDataContractName(type);
-            string typeNamespaceString = GetDataContractNamespace(type);
-
-            if (typeNameString == null || typeNamespaceString == null) {
-                return knownTypeResolver.TryResolveType(type, declaredType, knownTypeResolver, out typeName, out typeNamespace);
-            }
-
-            typeName = dictionary.Add(typeNameString);
-            typeNamespace = dictionary.Add(typeNamespaceString);
-
-            return true;
-        }
-
-        private static string GetDataContractNamespace(Type type) {
-            var dataContractAttributes = type.GetCustomAttributes(typeof(DataContractAttribute), true).OfType<DataContractAttribute>();
-            var namespaceAttr = dataContractAttributes.FirstOrDefault((attr) => attr.Namespace != null);
-
-            if (namespaceAttr != null) {
-                return namespaceAttr.Namespace;
-            }
-
-            var namespaceAttributes = type.Assembly.GetCustomAttributes(typeof(ContractNamespaceAttribute), false).OfType<ContractNamespaceAttribute>();
-            var namespaceAttr2 = namespaceAttributes.FirstOrDefault((attr) => attr.ClrNamespace == type.Namespace);
-
-            return namespaceAttr2 == null ? null : namespaceAttr2.ContractNamespace;
-        }
-
-        private static string GetDataContractName(Type type) {
-            var dataContractAttributes = type.GetCustomAttributes(typeof(DataContractAttribute), true).OfType<DataContractAttribute>();
-            var nameAttr = dataContractAttributes.FirstOrDefault((attr) => attr.Name != null);
-
-            return nameAttr == null ? null : nameAttr.Name;
-        }
-
-        public override Type ResolveName(string typeName, string typeNamespace, Type declaredType, DataContractResolver knownTypeResolver) {
-            var assemblies = GetNamespaceAssemblies(typeNamespace);
-
-            foreach (var assembly in assemblies) {
-                var type = assembly.GetTypes().Where((t) => IsTypeContractMatch(t, typeName, typeNamespace)).SingleOrDefault();
-
-                if (type != null) {
-                    return type;
-                }
-            }
-
-            return knownTypeResolver.ResolveName(typeName, typeNamespace, declaredType, knownTypeResolver);
-        }
-
-        private static bool IsTypeContractMatch(Type type, string typeName, string typeNamespace) {
-            return GetDataContractName(type) == typeName && GetDataContractNamespace(type) == typeNamespace;
-        }
-
-        private IEnumerable<Assembly> GetNamespaceAssemblies(string typeNamespace) {
-            // TODO Clean up
-
-            var extension = this.extensionManager.Extensions.Where((ext) => ext.Namespace == typeNamespace).SingleOrDefault();
-
-            if (extension == null) {
-                // Not a type defined in an extension; test NoCap.Library.
-                if (typeNamespace == "http://strager.net/nocap/lib") {
-                    return new[] { typeof(NoCap.Library.ICommand).Assembly };
-                }
-
-                if (typeNamespace == "http://strager.net/nocap/gui") {
-                    return new[] { typeof(App).Assembly };
-                }
-
-                return Enumerable.Empty<Assembly>();
-            }
-
-            return extension.Assemblies;
-        }
-    }
-
-    class MySurrogate : IDataContractSurrogate {
-        public Type GetDataContractType(Type type) {
-            return type;
-        }
-
-        public object GetObjectToSerialize(object obj, Type targetType) {
-            // TODO Round-trip dummy
-
-            return obj;
-        }
-
-        public object GetDeserializedObject(object obj, Type targetType) {
-            // TODO Round-trip dummy
-
-            return obj;
-        }
-
-        public object GetCustomDataToExport(MemberInfo memberInfo, Type dataContractType) {
-            throw new NotSupportedException();
-        }
-
-        public object GetCustomDataToExport(Type clrType, Type dataContractType) {
-            throw new NotSupportedException();
-        }
-
-        public void GetKnownCustomDataTypes(Collection<Type> customDataTypes) {
-            // TODO?
-        }
-
-        public Type GetReferencedTypeOnImport(string typeName, string typeNamespace, object customData) {
-            return null;
-        }
-
-        public CodeTypeDeclaration ProcessImportedType(CodeTypeDeclaration typeDeclaration, CodeCompileUnit compileUnit) {
-            return typeDeclaration;
-        }
-    }
-
-    class MySurrogateSelector : ISurrogateSelector {
-        private ISurrogateSelector nextSelector;
-
-        public void ChainSelector(ISurrogateSelector selector) {
-            this.nextSelector = selector;
-        }
-
-        public ISerializationSurrogate GetSurrogate(Type type, StreamingContext context, out ISurrogateSelector selector) {
-            if (type.IsGenericType && type.GetInterfaces().Contains(typeof(IBindableCollection))) {
-                selector = this;
-
-                return new BindableCollectionSurrogate();
-            }
-
-            selector = null;
-
-            return this.nextSelector == null
-                ? null
-                : this.nextSelector.GetSurrogate(type, context, out selector);
-        }
-
-        public ISurrogateSelector GetNextSelector() {
-            return this.nextSelector;
-        }
-    }
-
-    class BindableCollectionSurrogate : ISerializationSurrogate {
-        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context) {
-            var bindableCollection = (IBindableCollection) obj;
-
-            var items = new ArrayList(bindableCollection.Count);
-
-            foreach (var item in bindableCollection) {
-                items.Add(item);
-            }
-
-            info.AddValue("Items", items);
-            info.AddValue("ItemType", obj.GetType());
-        }
-
-        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector) {
-            var items = info.GetValue<IEnumerable>("Items");
-            var type = info.GetValue<Type>("ItemType");
-
-            dynamic bindableCollection = Activator.CreateInstance(type, new object[] { });
-
-            foreach (var item in items) {
-                bindableCollection.Add(item);
-            }
-
-            return bindableCollection;
         }
     }
 }
