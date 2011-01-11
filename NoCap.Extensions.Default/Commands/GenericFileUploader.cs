cb480eff1d8d517a796de70d1c1dec7ddd8a8e73
strager <strager.nds@gmail.com>
2011-01-11 15:42:47 -0500

    UI for GenericFileUploader.

--- NoCap.Extensions.Default/Commands/GenericFileUploader.cs
+++ NoCap.Extensions.Default/Commands/GenericFileUploader.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Threading;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using Bindable.Linq.Collections;
 using NoCap.Library;
 using NoCap.Library.Progress;
 using NoCap.Library.Util;
 using NoCap.Web.Multipart;
 
 namespace NoCap.Extensions.Default.Commands {
+    class StringPair : IXmlSerializable {
+        public StringPair() {
+        }
+
+        public StringPair(string key, string value) {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key {
+            get;
+            set;
+        }
+
+        public string Value {
+            get;
+            set;
+        }
+
+        public XmlSchema GetSchema() {
+            return new XmlSchema();
+        }
+
+        public void ReadXml(XmlReader reader) {
+            reader.ReadStartElement();
+
+            Key = reader.ReadElementString("Key");
+            Value = reader.ReadElementString("Value");
+        }
+
+        public void WriteXml(XmlWriter writer) {
+            writer.WriteElementString("Key", Key);
+            writer.WriteElementString("Value", Value);
+        }
+    }
+
     [DataContract(Name = "GenericFileUploader")]
     sealed class GenericFileUploader : ICommand, INotifyPropertyChanged {
         private string name;
@@ -40,13 +79,13 @@ namespace NoCap.Extensions.Default.Commands {
         }
 
         [DataMember(Name = "PostParameters")]
-        public IDictionary<string, string> PostParameters {
+        public BindableCollection<StringPair> PostParameters {
             get;
             set;
         }
 
         public GenericFileUploader() {
-            PostParameters = new Dictionary<string, string>();
+            PostParameters = new BindableCollection<StringPair>();
         }
 
         public TypedData Process(TypedData data, IMutableProgressTracker progress, CancellationToken cancelToken) {
@@ -57,6 +96,10 @@ namespace NoCap.Extensions.Default.Commands {
             var dataBuilder = new MultipartDataBuilder();
 
             foreach (var parameter in PostParameters) {
+                if (parameter.Key == null) {
+                    continue;
+                }
+
                 dataBuilder.KeyValuePair(parameter.Key, StringLib.HartFormatter.HartFormat(parameter.Value, parameters));
             }
 
--- NoCap.Extensions.Default/Editors/GenericFileUploaderEditor.xaml.cs
+++ NoCap.Extensions.Default/Editors/GenericFileUploaderEditor.xaml.cs
@@ -1,12 +1,38 @@
-﻿using NoCap.Library;
+﻿using System.Windows.Input;
+using NoCap.Extensions.Default.Commands;
+using NoCap.Library;
 
 namespace NoCap.Extensions.Default.Editors {
     /// <summary>
     /// Interaction logic for GenericFileUploaderEditor.xaml
     /// </summary>
     public partial class GenericFileUploaderEditor : ICommandEditor {
+        private GenericFileUploader Command {
+            get {
+                return DataContext as GenericFileUploader;
+            }
+        }
+
         public GenericFileUploaderEditor() {
             InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, AddNewParameter));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Delete, DeleteParameter, CanDeleteParameter));
+        }
+
+        private void AddNewParameter(object sender, ExecutedRoutedEventArgs e) {
+            Command.PostParameters.Add(new StringPair(null, ""));
+        }
+
+        private void DeleteParameter(object sender, ExecutedRoutedEventArgs e) {
+            var kvp = (StringPair) this.parameterEditor.SelectedItem;
+
+            Command.PostParameters.Remove(kvp);
+        }
+
+        private void CanDeleteParameter(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = this.parameterEditor.SelectedIndex >= 0;
+            e.Handled = true;
         }
     }
 }
