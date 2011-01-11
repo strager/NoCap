fd3ac8fcdd5c7c9449434e97dcb4f7458acb21b7
strager <strager.nds@gmail.com>
2011-01-11 13:36:05 -0500

    min.us uploading support.

---
+++ NoCap.Extensions.Default/Factories/MinUsUploaderFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Composition;
+using NoCap.Extensions.Default.Commands;
+using NoCap.Library;
+
+namespace NoCap.Extensions.Default.Factories {
+    [Export(typeof(ICommandFactory))]
+    public class MinUsUploaderFactory : ICommandFactory {
+        public string Name {
+            get { return "min.us uploader"; }
+        }
+
+        public ICommand CreateCommand() {
+            return new MinUsUploader();
+        }
+
+        public void PopulateCommand(ICommand command, ICommandProvider commandProvider) {
+        }
+
+        public ICommandEditor GetCommandEditor(ICommandProvider commandProvider) {
+            return null;
+        }
+
+        public CommandFeatures CommandFeatures {
+            get { return CommandFeatures.FileUploader; }
+        }
+    }
+}
