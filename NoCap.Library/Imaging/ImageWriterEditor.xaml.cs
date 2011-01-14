807d2f23f65ae4097f2753bc8c0f92007dee5652
strager <strager.nds@gmail.com>
2011-01-14 12:08:37 -0500

    Fix: Image codecs do not appear in combo box
    https://www.pivotaltracker.com/story/show/8540835

--- NoCap.Library/Imaging/ImageWriterEditor.xaml.cs
+++ NoCap.Library/Imaging/ImageWriterEditor.xaml.cs
@@ -1,32 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace NoCap.Library.Imaging {
     /// <summary>
     /// Interaction logic for ImageWriterEditor.xaml
     /// </summary>
     public partial class ImageWriterEditor : ICommandEditor {
-        private readonly HashSet<BitmapCodecFactory> codecFactories;
-
-        public IEnumerable<BitmapCodecFactory> CodecFactories {
-            get {
-                return this.codecFactories;
-            }
-        }
-
-        public ImageWriterEditor(ICommandProvider commandProvider) :
-            this(commandProvider.GetBitmapCodecFactories()) {
-        }
-
-        public ImageWriterEditor(IEnumerable<BitmapCodecFactory> codecFactories) {
-            // Must be set before the InitializeCompoent call
-            // so bindings are set up against these (and not null)
-            this.codecFactories = new HashSet<BitmapCodecFactory>(codecFactories);
-
+        public ImageWriterEditor() {
             InitializeComponent();
 
-            this.codecSelector.Filter = new Predicate<ICommandFactory>((factory) => this.codecFactories.Contains(factory));
+            this.codecSelector.Filter = new Predicate<ICommandFactory>((factory) => factory is BitmapCodecFactory);
         }
     }
 }
